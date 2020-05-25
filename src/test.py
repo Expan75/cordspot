e0356bab1d625d65ed8f8d8e4ce7968c92f45c3a
stigsvensson <erik.hakansson96@gmail.com>
2020-05-25 14:26:15 +0100

    added more systematic testing /w POST request

--- src/test.py
+++ src/test.py
@@ -1,11 +1,20 @@
 """ Script for testing POST request /w file classification """
 from requests import post
+import os
 
-# get file
-TEST_PATH = "/Users/Erik/Dev/cordspot/data/test/test1.jpeg"
-
-# create request and send it
-res = post("http://127.0.0.1:5000/predict", files={"file": open(TEST_PATH, "rb")},)
+# get files
+TEST_PATH = "/Users/Erik/Dev/cordspot/data/test"
+subfolders = os.listdir(TEST_PATH)
+for label in subfolders:
+    # run predict on each file in them
+    label_path = os.path.join(TEST_PATH, label)
+    image_paths = [
+        os.path.join(label_path, relpath) for relpath in os.listdir(label_path)
+    ]
+    print("Running predict on %s" % label)
+    for image in image_paths:
+        res = post("http://127.0.0.1:5000/predict", files={"file": open(image, "rb")},)
+        print(res.text)
 
 # output
-print(res.json())
+# print(res.json())
